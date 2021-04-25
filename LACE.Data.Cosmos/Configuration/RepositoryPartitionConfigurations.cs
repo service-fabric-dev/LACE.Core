3ebd18620462ebd9ce208bfc9b6487d59f7353b2
Jace O'Brien <servicefabricdev@gmail.com>
2021-04-24 21:29:39 -0500

    Assign default on creation

--- LACE.Data.Cosmos/Configuration/RepositoryPartitionConfigurations.cs
+++ LACE.Data.Cosmos/Configuration/RepositoryPartitionConfigurations.cs
@@ -4,6 +4,6 @@ namespace LACE.Data.Cosmos.Configuration
 {
     public class RepositoryPartitionConfigurations : Dictionary<string, RepositoryPartitionConfiguration>
     {
-        public RepositoryPartitionConfiguration Default => ContainsKey(nameof(Default)) ? this[nameof(Default)] : new RepositoryPartitionConfiguration();
+        public RepositoryPartitionConfiguration Default => ContainsKey(nameof(Default)) ? this[nameof(Default)] : this[nameof(Default)] = new RepositoryPartitionConfiguration();
     }
 }
